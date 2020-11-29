036baf82fb1e06b55f414abfcd0666915b739d51
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2020-11-29 15:56:06 +0100

    Add new answers
    Adds new answers in the following categories:
    - Tech
    - Time
    
    And adds the new category 'Education'

--- bestpy/answers.py
+++ bestpy/answers.py
@@ -30,14 +30,35 @@ class CodeAnswers:
 answers = {
     # region: Tech
     "language": "Python",
+    "programming_language": "Python",
+    "pep": [0, 1, 5, 7, 8, 10, 11, 387, 20, 13, 101, 257, 287, 8016, 8000, 8002, 8102, 484],
     "package": "Bestpy",
     "phone": ["iPhone", "Nokia", "OnePlus", "Huawei", "Samsung Galaxy", "BlackBerry", "Google Pixel", "Sony Experia"],
+    "company": ["Apple", "Samsung", "Nokia", "Google", "Facebook", "Microsoft", "Sony", "Huawei", "IBM", "Intel",
+                "Adobe", "Cisco", "Dell", "Oracle", "Klurio"],
+    # endregion
+
+    # region: Education
+    "subject": ["computer science", "biology", "maths", "chemistry", "physics", "social sciences",
+                "natural sciences", "music", "language", "geography", "history", "dance", "design",
+                "physical education", "religion", "design", "psychology", "sociology", "health", "physiology"],
+    "teacher_name": ["Dave", "Mary", "Frank"],
+    "grade": "A",
+    "gpa": [4.0, 5.0],
+    "enter": 99.9,
+    "gcse": 9,
     # endregion
 
     # region: Time
     "year": list(range(2100)),
     "month": ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october",
               "november", "december"],
+    "date": list(range(1, 32)),
+    "weekday": ["monday", "tuesday", "wednesday", "thursday", "friday"],
+    "day": ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+    "time_period": ["stone age", "bronze age", "classical antiquity", "iron age", "renaissance", "industrial age",
+                    "atomic age", "space age", "information age"],
+    "week": list(range(1, 53)),
     # endregion
 }
 
