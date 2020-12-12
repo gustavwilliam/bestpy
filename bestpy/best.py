e9e8dc202fa46506541846bed6e029b7a77a9e46
SuperMaZingCoder <supermazingcoder@gmail.com>
2020-12-11 22:46:41 -0500

    Made code more readable.

--- bestpy/best.py
+++ bestpy/best.py
@@ -4,7 +4,7 @@ import random
 
 
 class Best:
-    def __getattr__(self, name):
+    def __getattribute__(self, name):
         # Return random value in list
         if type(answers[name]) != list:
             return answers[name]
