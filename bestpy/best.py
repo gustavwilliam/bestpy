07c4a6427d96bea73ca92c599fd69f90a9295bbf
SuperMaZingCoder <55718659+SuperMaZingCoder@users.noreply.github.com>
2020-12-15 14:46:51 -0500

    Functions can now be used as answer types.

--- bestpy/best.py
+++ bestpy/best.py
@@ -8,5 +8,10 @@ class Best:
         # Return random value in list
         if isinstance(answers[name], list):
             return random.choice(answers[name])
+        # Call function
+        elif isinstance(answers[name], function):
+            return answers[name]()
         else:
             return answers[name]
+      
+    
