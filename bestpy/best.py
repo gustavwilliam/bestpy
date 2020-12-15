e26538dececd3e9f3d126086fefaf6a549a3acf6
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2020-12-15 07:50:57 +0100

    Update bestpy/best.py
    Co-authored-by: vivax <51753506+vivax3794@users.noreply.github.com>

--- bestpy/best.py
+++ bestpy/best.py
@@ -6,7 +6,7 @@ import random
 class Best:
     def __getattribute__(self, name):
         # Return random value in list
-        if type(answers[name]) != list:
-            return answers[name]
-        else:
+        if isinstance(answers[name], list):
             return random.choice(answers[name])
+        else:
+            return answers[name]
