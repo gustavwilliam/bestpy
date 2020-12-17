d14483529dc0072f7c3d5bcf421441542d17dea9
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2020-12-18 03:12:27 +0530

    use ABC in isinstance check

--- bestpy/best.py
+++ bestpy/best.py
@@ -1,14 +1,16 @@
-from .answers import answers
-
 import random
+from collections import abc
+
+from .answers import answers
 
 
 class Best:
     def __getattribute__(self, name):
         answer = answers[name]
-        if callable(answer):
-            answer = answer()
 
-        if isinstance(answer, list):
+        if callable(answer):
+            return answer()
+        elif isinstance(answer, abc.Sequence):
             return random.choice(answer)
+
         return answer
