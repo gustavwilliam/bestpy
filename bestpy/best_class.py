3c90d1e875fe311df6d5752e298f91268a71923e
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-02-06 22:41:59 +0100

    Use the correct file for `Best` class
    Also ensures that answers of type `str` don't get treated as sequences.
    Otherwise, bestpy will return a random letter from the string.

--- bestpy/best_class.py
+++ bestpy/best_class.py
@@ -1,14 +1,18 @@
-from .answers import answers
-
 import random
+from collections import abc
+
+from .answers import answers
 
 
 class Best:
     def __getattribute__(self, name):
         answer = answers[name]
+
         if callable(answer):
             answer = answer()
+        elif isinstance(answer, str):
+            answer = answer
+        elif isinstance(answer, abc.Sequence):
+            answer = random.choice(answer)
 
-        if isinstance(answer, list):
-            return random.choice(answer)
         return answer
