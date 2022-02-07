c5bf755564d439802f2a4bef651a08bbc844b5bb
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-02-07 02:46:25 +0100

    Ensure that non-answer attributes are accessible
    Previously, `KeyError` was raised for invalid attribute access of the
    `Best` class. And the attributes that weren't injected due to being
    answers weren't accessible at all

--- bestpy/best_class.py
+++ bestpy/best_class.py
@@ -6,7 +6,11 @@ from .answers import answers
 
 class Best:
     def __getattribute__(self, name):
-        answer = answers[name]
+        answer = answers.get(name)
+        if answer is None:
+            # Ensure that attributes that aren't answers are also accessible
+            # and throw AttributeError if nothing is found
+            return object.__getattribute__(self, name)
 
         if callable(answer):
             answer = answer()
