9ea1f7f916b72b794819ecedc589c40da5387c68
Gustav Odinger <65498475+gustavwilliam@users.noreply.github.com>
2022-02-09 13:45:27 +0100

    Refactor __getattr__ into _extract_answers method
    This will be helpful for allowing different ways of getting answers from
    the class

--- bestpy/best_class.py
+++ bestpy/best_class.py
@@ -6,21 +6,36 @@ class Best:
     def __init__(self, answers):
         self._answers = answers
 
+    @staticmethod
+    def _extract_answer(raw_answer):
+        """
+        Extracts and returns the answer from a raw answer.
+
+        If `raw_answer` if callable, the return value is returned, and
+        if it's a sequence, one random element in the sequence is returned.
+        For other types, `raw_answer` is simply returned.
+        """
+        if callable(raw_answer):
+            return raw_answer()
+
+        elif isinstance(raw_answer, str):
+            # Has a special case, so it doesn't get matched as a sequence below.
+            # That would return a random letter in the string, which isn't desirable.
+            return raw_answer
+
+        elif isinstance(raw_answer, abc.Sequence):
+            # Return a random item in the sequence
+            return random.choice(raw_answer)
+
+        return raw_answer
+
     def __getattr__(self, name):
         try:
             answer = self._answers[name]
-            print(answer)
         except KeyError:
             error_message = f"'{self.__class__.__name__}' object has no attribute '{name}'"
 
             # `from None` ensures that the error raised does not originate from the `KeyError` above
             raise AttributeError(error_message) from None
 
-        if callable(answer):
-            answer = answer()
-        elif isinstance(answer, str):
-            answer = answer
-        elif isinstance(answer, abc.Sequence):
-            answer = random.choice(answer)
-
-        return answer
+        return self._extract_answer(answer)
